df2182ed392e884c6227b9564aff9f928ce122ec
ijacksun <anjacksun@gmail.com>
2023-03-29 18:27:52 -0300

    Criacao de repositorios

---
+++ src/main/java/com/jack/bookstore/repositories/LivroRepository.java
@@ -0,0 +1,11 @@
+package com.jack.bookstore.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jack.bookstore.domain.Livro;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer>{
+
+}
