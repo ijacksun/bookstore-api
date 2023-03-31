492280086fc955e51d7346930b2be6325f5716b6
ijacksun <anjacksun@gmail.com>
2023-03-30 22:07:06 -0300

    excecao personalizada para objeto nao encontrado

--- src/main/java/com/jack/bookstore/service/CategoriaService.java
+++ src/main/java/com/jack/bookstore/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.jack.bookstore.domain.Categoria;
 import com.jack.bookstore.repositories.CategoriaRepository;
+import com.jack.bookstore.service.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
@@ -16,7 +17,8 @@ public class CategoriaService {
 	
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = repository.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 }
