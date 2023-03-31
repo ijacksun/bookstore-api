ad69205889ea7664d53b303c87b9d4cdf0df2bf1
ijacksun <anjacksun@gmail.com>
2023-03-30 23:27:30 -0300

    Excessao personalizada para delete de categoria

--- src/main/java/com/jack/bookstore/service/CategoriaService.java
+++ src/main/java/com/jack/bookstore/service/CategoriaService.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.jack.bookstore.domain.Categoria;
@@ -41,7 +42,12 @@ public class CategoriaService {
 	
 	public void delete(Integer id) {
 		findById(id);
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new com.jack.bookstore.service.exceptions.DataIntegrityViolationException(
+					"Categoria não pode ser deletado! Possui livros associados.");
+		}
 	}
 
 }
