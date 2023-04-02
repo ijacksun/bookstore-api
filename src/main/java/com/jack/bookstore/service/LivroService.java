48e700c940cd32548dd794b4a3b4afaedb2ecce5
ijacksun <anjacksun@gmail.com>
2023-04-02 02:25:51 -0300

    POST para livro

--- src/main/java/com/jack/bookstore/service/LivroService.java
+++ src/main/java/com/jack/bookstore/service/LivroService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.jack.bookstore.domain.Categoria;
 import com.jack.bookstore.domain.Livro;
 import com.jack.bookstore.repositories.LivroRepository;
 import com.jack.bookstore.service.exceptions.ObjectNotFoundException;
@@ -40,5 +41,12 @@ public class LivroService {
 		newObj.setTitulo(obj.getTitulo());
 		newObj.setNome_autor(obj.getNome_autor());
 		newObj.setTexto(obj.getTexto());
+	}
+	
+	public Livro create(Integer id_cat, Livro obj) {
+		obj.setId(null);
+		Categoria cat = categoriaService.findById(id_cat);
+		obj.setCategoria(cat);
+		return repository.save(obj);
 	}
 }
